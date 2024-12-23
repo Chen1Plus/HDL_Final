e57268b7ed116a0cdae0406ab2b56bae99a3f1dd
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-12-23 21:11:13 +0800

    chore: code style

--- src/hdl/TopReceiver.v
+++ src/hdl/TopReceiver.v
@@ -1,13 +1,11 @@
-module TopReceiver(
-    input       rst,
-    input       clk,
-    input       computer_rx,
-    output      computer_tx,
-    output      bt2_rx,
-    input       bt2_tx,
-    output      cts
+module TopReceiver (
+    output pc_tx,
+    input  pc_rx,
+    output bt2_cts,
+    input  bt2_tx,
+    output bt2_rx
 );
-    assign cts = 0;
-    assign bt2_rx = computer_rx;
-    assign computer_tx = bt2_tx;
+    assign pc_tx   = bt2_tx;
+    assign bt2_cts = 1'b0;
+    assign bt2_rx  = pc_rx;
 endmodule : TopReceiver
